a926065f33f5ed2923385ea0bad3da75ca50f5dd
Mandy Idema <m.e.idema@st.hanze.nl>
2023-01-03 16:16:50 +0100

    Continious movement
    - Switched to XR Interaction toolkit

---
+++ Assets/animateHandonInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class animateHandonInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    //Creates variable for action in inspector
+
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+
+        //Read if input of trigger button is used
+
+        //Debug.Log(triggerValue);
+    }
+}
