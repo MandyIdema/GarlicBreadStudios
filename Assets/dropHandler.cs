80dba8d5adf1218ee4e9017da5db03e7102855d4
Mandy Idema <m.e.idema@st.hanze.nl>
2023-01-07 14:01:46 +0100

    Started on main mechanics
    - Dropping off point

---
+++ Assets/dropHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropHandler : MonoBehaviour
+{
+    public GameObject[] prizes;
+    public GameObject[] dropPoint;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
