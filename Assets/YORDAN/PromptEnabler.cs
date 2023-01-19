127c4a58a08ca0da5aed6560c52cf66b905d4cef
wavyxd <dabestbg@gmail.com>
2023-01-19 15:46:51 +0100

    Prompts + light beginning

--- Assets/YORDAN/PromptEnabler.cs
+++ Assets/YORDAN/PromptEnabler.cs
@@ -6,12 +6,40 @@ using UnityEngine.UI;
 public class PromptEnabler : MonoBehaviour
 {
     public GameObject canvas;
+    public GameObject canvas1;
+    public GameObject canvas2;
+    public GameObject canvas3;
+    public GameObject canvas4;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "promptTrigger")
         {
             canvas.SetActive(true);
+            Debug.Log("triggered");
+        }
+
+        if (other.tag == "promptTrig")
+        {
+            canvas1.SetActive(true);
+            Debug.Log("triggered");
+        }
+
+        if (other.tag == "canvas2")
+        {
+            canvas2.SetActive(true);
+            Debug.Log("triggered");
+        }
+
+        if (other.tag == "canvas3")
+        {
+            canvas3.SetActive(true);
+            Debug.Log("triggered");
+        }
+
+        if (other.tag == "canvas4")
+        {
+            canvas4.SetActive(true);
             Debug.Log("triggered");
         }
     }
