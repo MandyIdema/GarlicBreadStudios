b6b6dc44309de8319ae481033c39e5127292ca2f
wavyxd <dabestbg@gmail.com>
2023-01-23 18:49:54 +0100

    Museum Audio Finished

--- Assets/YORDAN/AudioEnabler.cs
+++ Assets/YORDAN/AudioEnabler.cs
@@ -4,36 +4,72 @@ using UnityEngine;
 
 public class AudioEnabler : MonoBehaviour
 {
-
     public AudioSource promptOne;
     public AudioSource promptTwo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //promptOne = GetComponent<AudioSource>();
-    }
-
+    public AudioSource promptThree;
+    public AudioSource promptFour;
+    public AudioSource promptFive;
+    public AudioSource promptSix;
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "promptTrigger")
         {
             promptOne.Play();
-            //Destroy(other.gameObject);
+            promptTwo.Stop();
+            promptThree.Stop();
+            promptFour.Stop();
+            promptFive.Stop();
+            promptSix.Stop();
         }
 
         if (other.gameObject.tag == "promptTrig")
         {
             promptTwo.Play();
             promptOne.Stop();
-            //Destroy(other.gameObject);
+            promptThree.Stop();
+            promptFour.Stop();
+            promptFive.Stop();
+            promptSix.Stop();
         }
-    }
 
-    
+        if (other.gameObject.tag == "canvas2")
+        {
+            promptTwo.Stop();
+            promptOne.Stop();
+            promptThree.Play();
+            promptFour.Stop();
+            promptFive.Stop();
+            promptSix.Stop();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "canvas3")
+        {
+            promptTwo.Stop();
+            promptOne.Stop();
+            promptThree.Stop();
+            promptFour.Play();
+            promptFive.Stop();
+            promptSix.Stop();
+        }
+
+        if (other.gameObject.tag == "canvas4")
+        {
+            promptTwo.Stop();
+            promptOne.Stop();
+            promptThree.Stop();
+            promptFour.Stop();
+            promptSix.Stop();
+            promptFive.Play();
+        }
+
+        if (other.gameObject.tag == "canvas5")
+        {
+            promptTwo.Stop();
+            promptOne.Stop();
+            promptThree.Stop();
+            promptFour.Stop();
+            promptFive.Stop();
+            promptSix.Play();
+        }
     }
 }
